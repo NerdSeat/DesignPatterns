5c0894e3c66a2dbbc60921d9b6efce7da53c660f
Joseph <josephblister@gmail.com>
2025-03-15 23:59:37 +0200

    remove filesA

--- Singleton/ThreadSafeSingleton/Main.java
+++
@@ -1,9 +0,0 @@
-/*
- * Sanlam Fintech (c) 2025. All rights reserved.
- * Unauthorized copying of this file, via any medium  is strictly prohibited.
- * Proprietary and confidential.
- */
-
-public class Main {
-
-}
